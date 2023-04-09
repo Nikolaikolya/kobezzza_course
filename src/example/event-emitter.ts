1a8d76ad017d4188e10347ff55eb62835a893019
Nikolaikolya <starcrafters@ya.ru>
2023-04-09 21:44:03 +0700

    delete example dir

--- src/example/event-emitter.ts
+++
@@ -1,9 +0,0 @@
-import { EventEmitter } from "../library/event-emitter";
-import { DOMEventEmitter } from "../library/event-emitter/dom-event-emitter";
-const dee = new DOMEventEmitter();
-
-const ee = new EventEmitter(dee);
-
-ee.on({ action: () => {}, event: "click", target: "a" });
-
-ee.emit({ args: [], event: "click" });
